67c082aa0d2577eb2fdb112992e88c02b6a368ec
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-08-04 15:03:56 +0300

    feat: change count of pizza

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,28 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
+
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  function handleDecreaseItemQuantity() {
+    dispatch(decreaseItemQuantity(pizzaId));
+  }
+
+  function handleIncreaseItemQuantity() {
+    dispatch(increaseItemQuantity(pizzaId));
+  }
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={handleDecreaseItemQuantity}>
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button type="round" onClick={handleIncreaseItemQuantity}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
